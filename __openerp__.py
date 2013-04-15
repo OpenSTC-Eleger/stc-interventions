4bd5b046c12ba30fe6ec1a741ac7fc6f7d64b647
plancher bruno <bruno.plancher@gmail.com>
2013-04-15 10:49:44 +0200

    same

--- __openerp__.py
+++ __openerp__.py
@@ -10,7 +10,7 @@
     "version": "0.1",
     "depends": ["purchase", "project", "board","product", "stock", "hotel_reservation", "email_template"],
     "author": "PYF & BP",
-    "shortdesc":"openstc for prets achats only",
+    "shortdesc":"openstc_prets",
     "category": "Category",
     "description": """
     Module STC For Prêts, temporary module to store changes specific to prets untested features on SWIF 
