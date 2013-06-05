36f1dbd8a7ff735c095a15dbd34c21400cbc78d8
Cedric Brancourt <cedric.brancourt@gmail.com>
2013-06-05 16:13:36 +0200

    Added AGPL Licence in files see commit message below
    copyright-header --license AGPL3 --add-path ./ --guess-extension --copyright-holder 'Siclic www.siclic.fr' --copyright-software 'OpenSTC Interventions' --copyright-software-description "Openerp Module to manage Cityhall technical department" --copyright-year 2013 -o ./
    SKIP .//openstc_pret.py; detected existing license
    SKIP .//workflow; not file
    UPDATE .//workflow/ask.xml
    UPDATE .//workflow/purchase_workflow.xml
    UPDATE .//workflow/openstc_pret_emprunt_workflow.xml
    UPDATE .//workflow/openstc_pret_workflow.xml
    UPDATE .//__openerp__.py
    SKIP .//openstc.py; detected existing license
    SKIP .//views; not file
    UPDATE .//views/openstc_view.xml
    UPDATE .//views/openstc_pret_menus_view.xml
    UPDATE .//views/openstc_pret_checkout_view.xml
    UPDATE .//views/openstc_pret_view.xml
    UPDATE .//views/openstc_pret_data.xml
    SKIP .//stc-interventions.iml; unsupported
    SKIP .//wizard; not file
    SKIP .//wizard/openstc_pret_wizard.py; detected existing license
    UPDATE .//wizard/ask_refused.py
    SKIP .//wizard/create_task.py; detected existing license
    UPDATE .//wizard/ask_refused_view.xml
    UPDATE .//wizard/create_task_view.xml
    UPDATE .//wizard/openstc_pret_view_wizard.xml
    SKIP .//wizard/__init__.py; detected existing license
    UPDATE .//wizard/ask_modify_service.xml
    SKIP .//wizard/ask_modify_service.py; detected existing license
    SKIP .//report; not file
    UPDATE .//report/openstc_pret_qte_dispo_report_view.xml
    SKIP .//report/openstc_pret_qte_dispo_report.py; detected existing license
    SKIP .//report/__init__.py; detected existing license
    SKIP .//test; not file
    SKIP .//test/openstc_prets_tests.yml; unsupported
    SKIP .//security; not file
    UPDATE .//security/openstc_security.xml
    SKIP .//security/ir.model.access.csv; unsupported
    SKIP .//hotel.py; detected existing license
    SKIP .//README; undefined method `primary_extension' for nil:NilClass
    SKIP .//openstc_pret_checkout.py; detected existing license
    SKIP .//__init__.py; detected existing license
    SKIP .//static; not file
    SKIP .//static/js; not file
    UPDATE .//static/js/calendar_inherit.js
    SKIP .//i18n; not file
    SKIP .//i18n/fr_old.po; unsupported
    SKIP .//i18n/openstc.pot; unsupported
    SKIP .//i18n/fr.po; unsupported
    SKIP .//copyright.txt; unsupported
    SKIP .//unit_tests; not file
    UPDATE .//unit_tests/unit_tests.xml

--- static/js/calendar_inherit.js
+++ static/js/calendar_inherit.js
@@ -1,3 +1,23 @@
+/*
+ * OpenSTC Interventions - Openerp Module to manage Cityhall technical department
+ * Copyright (C) 2013 Siclic www.siclic.fr
+ *
+ * This file is part of OpenSTC Interventions.
+ *
+ * OpenSTC Interventions is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * OpenSTC Interventions is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenSTC Interventions.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 openerp.openstc_prets = function(openerp) {
 	t = openerp.web._t,
     _lt = openerp.web._lt;
