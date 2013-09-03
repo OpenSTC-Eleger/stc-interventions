b3016605cfff3733d5a37a940e18771ac7001791
plancher bruno <bruno.plancher@gmail.com>
2013-09-03 11:27:46 +0200

    added actions field on absenceType

--- openstc_inter.py
+++ openstc_inter.py
@@ -935,10 +935,31 @@ openstc_task_category()
 class openstc_absent_type(osv.osv):
     _name = "openstc.absent.type"
     _description = ""
+    
+    _actions = {
+        'delete':lambda self,cr,uid,record, groups_code: 'DIRE' in groups_code,
+        'update': lambda self,cr,uid,record, groups_code: 'MANA' in groups_code or 'DIRE' in groups_code,
+        'create': lambda self,cr,uid,record,groups_code: 'MANA' in groups_code or 'DIRE' in groups_code,
+
+    }
+
+    def _get_actions(self, cr, uid, ids, myFields ,arg, context=None):
+        #default value: empty string for each id
+        ret = {}.fromkeys(ids,'')
+        groups_code = []
+        groups_code = [group.code for group in self.pool.get("res.users").browse(cr, uid, uid, context=context).groups_id if group.code]
+
+        #evaluation of each _actions item, if test returns True, adds key to actions possible for this record
+        for record in self.browse(cr, uid, ids, context=context):
+            #ret.update({inter['id']:','.join([key for key,func in self._actions.items() if func(self,cr,uid,inter)])})
+            ret.update({record.id:[key for key,func in self._actions.items() if func(self,cr,uid,record,groups_code)]})
+        return ret
     _columns = {
             'name': fields.char('Affectation ', size=128, required=True),
             'code': fields.char('Code affectation', size=32, required=True),
             'description': fields.text('Description'),
+            'actions':fields.function(_get_actions, method=True, string="Actions possibles",type="char", store=False),
+
     }
 openstc_absent_type()
 
