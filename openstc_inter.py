f636e9cbcbeea439ebad0fb646991b0b1fadd1c5
pyfadet <pierreyves.fadet@siclic.fr>
2013-09-23 17:20:46 +0200

    BUG fix actions in project_project : add possibility to unplan pending intervention

--- openstc_inter.py
+++ openstc_inter.py
@@ -1150,7 +1150,7 @@ class project(osv.osv):
 
     _actions = {
         'cancel':lambda self,cr,uid,record: record.state in ('open','scheduled'),
-        'plan_unplan':lambda self,cr,uid,record: (record.state == 'open' and not self.pool.get("project.task").search(cr, uid,[('state','=','draft'),('project_id','=',record.id)])) or record.state == 'pending' ,
+        'plan_unplan':lambda self,cr,uid,record: (record.state == 'open' and not self.pool.get("project.task").search(cr, uid,[('state','=','draft'),('project_id','=',record.id)])) or record.state == 'scheduled' ,
         'add_task':lambda self,cr,uid,record: record.state in ('open','template'),
         'print': lambda self,cr,uid,record: True,
         'modify': lambda self,cr,uid,record: True,
