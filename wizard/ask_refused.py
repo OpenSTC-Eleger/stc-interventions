709a8f517501c2cba5a2b6ab6433e4d05a1e4193
plancher bruno <bruno.plancher@gmail.com>
2014-02-21 17:20:11 +0100

    updated header files to add AGPL licence on python files

--- wizard/ask_refused.py
+++ wizard/ask_refused.py
@@ -1,21 +1,22 @@
+# -*- coding: utf-8 -*-
 
-# OpenSTC Interventions - Openerp Module to manage Cityhall technical department
-# Copyright (C) 2013 Siclic www.siclic.fr
+##############################################################################
+#    Copyright (C) 2012 SICLIC http://siclic.fr
 #
-# This file is part of OpenSTC Interventions.
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
-# OpenSTC Interventions is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
 #
-# OpenSTC Interventions is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
 #
-# You should have received a copy of the GNU Affero General Public License
-# along with OpenSTC Interventions.  If not, see <http://www.gnu.org/licenses/>.
+#############################################################################
 
 from osv import osv
 from osv import fields
